9531af150c1784c9ab3b168612570e521d7ec604
Tim-ats-d <tim.arnouts@protonmail.com>
2026-01-28 16:50:56 +0100

    Progress.

--- ox/shared.ml
+++ ox/shared.ml
@@ -80,10 +80,6 @@ let send_and_wait t msg =
                 value := msg)
           in
           Mutex.Condition.signal global_cond;
-          let key =
-            Mutex.Condition.wait await global_cond ~lock:global_mutex key
-          in
-
           let rec loop key =
             let #(msg, key) =
               Capsule.Expert.Key.access key ~f:(fun access ->
